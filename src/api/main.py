bd146eb79e9aa5bd501b1f1bacb70a98b049174d
daniel.syahputra <daniel.syahputra@ui.ac.id>
2024-02-12 08:22:15 +0000

    initial commit

---
+++ src/api/main.py
@@ -0,0 +1,14 @@
+import pyrootutils
+
+ROOT = pyrootutils.setup_root(
+    search_from=__file__,
+    indicator=[".git"],
+    pythonpath=True,
+    dotenv=True,
+)
+
+import asyncio
+
+import hydra
+from omegaconf import DictConfig
+
